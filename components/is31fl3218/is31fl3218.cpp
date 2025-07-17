150273575b4cce76dd7c97e000290bf5ee1f93d4
MattiaBasso <125154980+MattiaBasso@users.noreply.github.com>
2025-07-17 10:54:59 +0200

    jjjj

--- components/is31fl3218/is31fl3218.cpp
+++ components/is31fl3218/is31fl3218.cpp
@@ -4,14 +4,13 @@
 namespace esphome {
 namespace is31fl3218 {
 
-static const uint8_t SHUTDOWN_REG = 0x00;
-static const uint8_t UPDATE_REG = 0x16;
-static const uint8_t RESET_REG = 0x17;
-static const uint8_t LED_CTRL_REG_1 = 0x13;
-static const uint8_t LED_CTRL_REG_2 = 0x14;
-static const uint8_t LED_CTRL_REG_3 = 0x15;
-
-static const uint8_t LDR_ON = 0x3F;
+uint8_t SHUTDOWN_REG = 0x00;
+uint8_t UPDATE_REG = 0x16;
+uint8_t RESET_REG = 0x17;
+uint8_t LED_CTRL_REG_1 = 0x13;
+uint8_t LED_CTRL_REG_2 = 0x14;
+uint8_t LED_CTRL_REG_3 = 0x15;
+uint8_t LDR_ON = 0x3F;
 
 static const char *TAG = "is31fl3218.component";
 
@@ -20,19 +19,19 @@ void IS31FL3218::setup() {
     // Note that a number of read/write methods are available in the I2CDevice
     // class. See "i2c/i2c.h" for details.
     ESP_LOGCONFIG(TAG, "Setting up IS31FL3218...");
-    if (!this->write(0x17, 0x00) != i2c::ERROR_OK) {
+    if (this->write(&RESET_REG, 0x00) != i2c::ERROR_OK) {
         this->mark_failed(); // Mark the component as failed if communication fails
         return;
     }
 
-    if (!this->write(SHUTDOWN_REG, 0x01) != i2c::ERROR_OK) {
+    if (this->write(&SHUTDOWN_REG, 0x01) != i2c::ERROR_OK) {
         this->mark_failed(); // Mark the component as failed if communication fails
         return;
     }
 
-    if ((!this->write(LED_CTRL_REG_1, LDR_ON) != i2c::ERROR_OK) && 
-       (!this->write(LED_CTRL_REG_2, LDR_ON) != i2c::ERROR_OK) && 
-       (!this->write(LED_CTRL_REG_3, LDR_ON) != i2c::ERROR_OK)){
+    if ((this->write(&LED_CTRL_REG_1, LDR_ON) != i2c::ERROR_OK) && 
+       (this->write(&LED_CTRL_REG_2, LDR_ON) != i2c::ERROR_OK) && 
+       (this->write(&LED_CTRL_REG_3, LDR_ON) != i2c::ERROR_OK)){
         this->mark_failed(); // Mark the component as failed if communication fails
         return;
     }    
