c8abd933e78d4941aed59a87d6951632373ec0f1
MattiaBasso <125154980+MattiaBasso@users.noreply.github.com>
2025-07-17 12:46:06 +0200

    bbbb

--- components/is31fl3218_output/is31fl3218.cpp
+++ components/is31fl3218_output/is31fl3218.cpp
@@ -7,7 +7,24 @@ namespace is31fl3218 {
 static const char *TAG = "is31fl3218.output";
 
 void IS31FL3218::setup(){
+    ESP_LOGCONFIG(TAG, "Setting up IS31FL3218...");
+    if (this->write(&RESET_REG, 0x00) != i2c::ERROR_OK) {
+        this->mark_failed(); // Mark the component as failed if communication fails
+        return;
+    }
 
+    if (this->write(&SHUTDOWN_REG, 0x01) != i2c::ERROR_OK) {
+        this->mark_failed(); // Mark the component as failed if communication fails
+        return;
+    }
+
+    if ((this->write(&LED_CTRL_REG_1, &LDR_ON) != i2c::ERROR_OK) && 
+       (this->write(&LED_CTRL_REG_2, &LDR_ON) != i2c::ERROR_OK) && 
+       (this->write(&LED_CTRL_REG_3, &LDR_ON) != i2c::ERROR_OK)){
+        this->mark_failed(); // Mark the component as failed if communication fails
+        return;
+    }    
+    ESP_LOGCONFIG(TAG, "Done setting up IS31FL3218 component.");
 }
 
 void IS31FL3218::write_state(float state){
